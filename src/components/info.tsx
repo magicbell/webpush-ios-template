662960bdf90b39fe12977cbb9b3425c8107f7859
Matt Oxley <matt_ox@hotmail.com>
2023-06-07 17:32:38 +0200

    include info component

---
+++ src/components/info.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react"
+import { deviceDetect } from "react-device-detect"
+
+// This component shows PWA-push related information to the user
+
+export default function Info() {
+  const [info, setInfo] = useState({})
+
+  useEffect(() => {
+    // put this in a useEffect to avoid SSR errors
+    const info = {
+      [`window.matchMedia('(display-mode: standalone)').matches`]:
+        window.matchMedia("(display-mode: standalone)").matches,
+      [`"standalone" in window.navigator`]: "standalone" in window.navigator,
+      [`/iPad|iPhone|iPod/.test(navigator.userAgent)`]: /iPad|iPhone|iPod/.test(
+        navigator.userAgent
+      ),
+    }
+    Object.assign(info, deviceDetect(window.navigator.userAgent))
+    setInfo(info)
+  }, [])
+  return (
+    <div>
+      <ul className="grid grid-cols-2 gap-x-8 gap-y-2">
+        {Object.entries(info).map(([key, value]) => (
+          <li key={key} className="contents">
+            <code className="text-right">{key}</code>{" "}
+            <code>{String(value)}</code>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Info from "@/components/info"
 import Subscriber from "@/components/subscriber"
 import { MagicBellProvider } from "@magicbell/react-headless"
 
@@ -9,6 +10,7 @@ export default function MyComponent() {
       userKey={process.env.NEXT_PUBLIC_MAGICBELL_USER_KEY as string}
     >
       <Subscriber />
+      <Info />
     </MagicBellProvider>
   )
 }
