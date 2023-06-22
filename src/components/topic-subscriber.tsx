1603701531d0745aaa75cea0558901234896d56a
Matt Oxley <matt_ox@hotmail.com>
2023-06-22 13:49:26 +0200

    fix

--- src/components/topic-subscriber.tsx
+++ src/components/topic-subscriber.tsx
@@ -45,10 +45,7 @@ export default function TopicSubscriber(
         disabled={props.idle || subscribing || unsubscribing}
         type="button"
         className={
-          "text-left inline-flex items-center gap-x-2 rounded-md border-primary border-2 px-3.5 py-2.5 text-xs font-semibold text-white shadow-sm hover:enabled:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50 " +
-          (isSubscribed
-            ? "bg-primary hover:enabled:bg-transparent"
-            : "bg-transparent")
+          "hover-button text-left inline-flex items-center gap-x-2 rounded-md px-3.5 py-2.5 text-xs font-semibold text-white shadow-sm bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:opacity-50"
         }
         onClick={isSubscribed ? handleUnsubscribe : handleSubscribe}
       >
