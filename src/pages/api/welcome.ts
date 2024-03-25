9c0a6d5ca2177a3827f4fc6b622c047cf43f5c10
Hana Mohan <unamashana@gmail.com>
2024-03-25 17:47:41 +0100

    updates the welcome message

--- src/pages/api/welcome.ts
+++ src/pages/api/welcome.ts
@@ -1,30 +1,31 @@
-import MagicBell from "magicbell"
-import type { NextApiRequest, NextApiResponse } from "next"
+import MagicBell from "magicbell";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 interface WelcomeRequest extends NextApiRequest {
   body: {
-    userId: string
-  }
+    userId: string;
+  };
 }
 
 type ResponseData = {
-  status: string
-}
+  status: string;
+};
 
 const magicbell = new MagicBell({
   apiKey: process.env.NEXT_PUBLIC_MAGICBELL_API_KEY,
   apiSecret: process.env.MAGICBELL_API_SECRET,
-})
+});
 
 export default async function handler(
   req: WelcomeRequest,
   res: NextApiResponse<ResponseData>
 ) {
   await magicbell.notifications.create({
-    title: "Thanks for subscribing!",
-    action_url: "https://magicbell.com",
+    title: "Ding, dong 🔔 Hello from MagicBell!",
+    content: "Lets add web push notifications to your app?",
+    action_url: "https://www.magicbell.com?utm_source=webpushtest",
     recipients: [{ external_id: req.body.userId }],
     category: "default",
-  })
-  res.status(200).json({ status: "success" })
+  });
+  res.status(200).json({ status: "success" });
 }
