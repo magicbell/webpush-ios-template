e61ec4e92f1f07b528652f0f9fbba4a3af151502
niya <niyabits@gmail.com>
2025-07-17 18:21:36 +0530

    fix: escape quotes in intall prompt

--- src/components/InstallPrompt.tsx
+++ src/components/InstallPrompt.tsx
@@ -24,7 +24,7 @@ export default function InstallPrompt() {
           {" "}
           ⎋{" "}
         </span>
-        and then "Add to Home Screen"
+        and then &quot;Add to Home Screen&quot;
         <span role="img" aria-label="plus icon">
           {" "}
           ➕{" "}
